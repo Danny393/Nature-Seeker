30644e4274bb6a85f1b623848e0d4ec2f3d8ba49
Danny393 <danielacosta393@gmail.com>
2020-05-29 22:42:47 -0400

    added basic parameter retrieval

--- src/Components/ItemDetails/ItemPage.js
+++ src/Components/ItemDetails/ItemPage.js
@@ -1,16 +1,46 @@
 import React from 'react';
-import {Container, Row} from 'react-bootstrap';
+import {Container, Row, Col} from 'react-bootstrap';
+
+const data = require("../models/fake-data");
 
 class ItemPage extends React.Component
 {
+    constructor(props){
+
+        super(props);
+        this.state = {
+          data: props.match.params
+        };
+
+        this.getDataFromDB = this.getDataFromDB.bind(this);
+      }
+    
+      getDataFromDB(){
+        if (this.state.data.hasOwnProperty('shoeSKU')) {
+          var shoe;
+          for(shoe of data.shoes){
+            if(shoe.sku === this.state.data.shoeSKU){
+              this.setState({data: shoe});
+              break;
+            }
+          }
+        } else if (this.state.data.hasOwnProperty('shirtSKU')) {
+          var shirt;
+          for(shirt of data.shirts){
+            if(shirt.sku === this.state.data.shirtSKU){
+              this.setState({data: shirt});
+              break;
+            }
+          }
+        }
+      }
+
     render()
     {
-        console.log("Hello World");
+        console.log(this.state.data);
         return(
             
-            <Container>
-                <p>"Hello World"</p>
-            </Container>
+            <p></p>
             
         );
     }
