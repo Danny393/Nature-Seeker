02f872acda8d5c866e75556deb8ba152a5b22655
Danny393 <danielacosta393@gmail.com>
2020-09-12 07:26:16 -0400

    fixed issue with windows and npm scripts with path

--- src/Components/models/SQLSchema.js
+++ src/Components/models/SQLSchema.js
@@ -4,7 +4,7 @@ const data = require('./fake-data');
 const uuid = require('uuid/v4');
 
 const Connection = new Sequelize (
-    'react-db',
+    'storeDB',
     'postgres',
     'postgres',
     {
@@ -12,3 +12,36 @@ const Connection = new Sequelize (
         host: 'localhost'
     }
 );
+
+//================== Tables ========================//
+const Users = Connection.define('users', {
+    id: {
+      allowNull: false,
+      primaryKey: true,
+      type: Sequelize.UUID,
+      defaultValue: Sequelize.UUID4
+    },
+    firstName: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    lastName: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    email: {
+      type: Sequelize.STRING,
+      unique: true,
+      allowNull: false,
+      validate: {
+        isEmail: true,
+      }
+    },
+    password: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      validate: {
+        isPassword: true,
+      }
+    }
+  });
